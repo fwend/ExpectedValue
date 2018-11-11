f5a7e03570ad0eaa2ab07005a0d6ec6c4ac2e52c
Jos van Uden <josvanuden@gmail.com>
2018-11-11 10:51:01 +0100

    improved commentary

--- README.md
+++ README.md
@@ -1,11 +1,19 @@
 # ExpectedValue
 Calculate the expected value of a fair die.<br>
-Should approach (1 + 2 + 3 + 4 + 5 + 6) / 6 = 3.5
+Should approach (1 + 2 + 3 + 4 + 5 + 6) * 1/6
 
 # Output
 
-3.50922<br>
-3.50215<br>
-3.50162<br>
-3.49815<br>
-3.49719<br>
+Expected value for one roll
+3.50009
+3.49428
+3.49248
+3.49859
+3.49779
+
+Expected value for two rolls (and keeping the highest)
+4.46603
+4.4711
+4.47094
+4.47479
+4.47025
--- main.js
+++ main.js
@@ -1,33 +1,30 @@
 const throwDie = () => Math.floor((Math.random() * 6) + 1)
 
-const calcExpectedValue = () => {
+const calcExpectedValue = (numRolls) => {
     const numThrows = 100000;
-    let avg = 0;
-    for (let i = 0; i < numThrows; i++) {
-        avg += throwDie();
-    }
-    return avg / numThrows;
-}
 
-const calcExpectedValue2 = () => {
-    const numThrows = 100000;
     let avg = 0;
     for (let i = 0; i < numThrows; i++) {
-        const result1 = throwDie();
-        const result2 = throwDie();
-        avg += Math.max(result1, result2);
+
+        let max = 0;
+        for (let j = 0; j < numRolls; j++) {
+            max = Math.max(max, throwDie());            
+        }
+
+        avg += max;
     }
+
     return avg / numThrows;
 }
 
 console.log("Expected value for one roll");
 for (let i = 0; i < 5; i++) {
-    console.log(calcExpectedValue());
+    console.log(calcExpectedValue(1));
 }
 
-console.log();
+console.log("\n");
 
 console.log("Expected value for two rolls");
 for (let i = 0; i < 5; i++) {
-    console.log(calcExpectedValue2());
+    console.log(calcExpectedValue(2));
 }
