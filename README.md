32d03f0b1170300ca48b7a98b634933f6357fc89
Jos van Uden <josvanuden@gmail.com>
2018-11-11 21:41:31 +0100

    simplified

--- README.md
+++ README.md
@@ -4,16 +4,8 @@ Should approach (1 + 2 + 3 + 4 + 5 + 6) / 6
 
 # Output
 
-Expected value for one roll<br>
 3.50009<br>
 3.49428<br>
 3.49248<br>
 3.49859<br>
 3.49779<br>
-
-Expected value for two rolls (and keeping the highest)<br>
-4.46603<br>
-4.4711<br>
-4.47094<br>
-4.47479<br>
-4.47025<br>
--- main.js
+++ main.js
@@ -1,30 +1,16 @@
 const throwDie = () => Math.floor((Math.random() * 6) + 1)
 
-const calcExpectedValue = (numRolls) => {
+const calcExpectedValue = () => {
     const numThrows = 100000;
 
     let avg = 0;
     for (let i = 0; i < numThrows; i++) {
-
-        let max = 0;
-        for (let j = 0; j < numRolls; j++) {
-            max = Math.max(max, throwDie());            
-        }
-
-        avg += max;
+       avg += throwDie();
     }
 
     return avg / numThrows;
 }
 
-console.log("Expected value for one roll");
 for (let i = 0; i < 5; i++) {
-    console.log(calcExpectedValue(1));
-}
-
-console.log("\n");
-
-console.log("Expected value for two rolls (and keeping the highest)");
-for (let i = 0; i < 5; i++) {
-    console.log(calcExpectedValue(2));
+    console.log(calcExpectedValue());
 }
