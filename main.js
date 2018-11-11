3e094724ff83bbdbe47aab8365bd979a0a3fe0dd
Jos van Uden <josvanuden@gmail.com>
2018-11-11 09:35:59 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+const throwDie = () => Math.floor((Math.random() * 6) + 1)
+
+const calcExpectedValue = () => {
+    const numThrows = 100000;
+    let avg = 0;
+    for (let i = 0; i < numThrows; i++) {
+        avg += throwDie();
+    }
+    return avg / numThrows;
+}
+
+for (let i = 0; i < 5; i++) {
+    console.log(calcExpectedValue());
+}
