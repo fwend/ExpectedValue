bb9aae5a0ee0c108add51cf627434cb4389bc709
Jos van Uden <josvanuden@gmail.com>
2019-01-24 23:03:55 +0100

    added expected value for 2 throws, if you keep the highest value

--- main.js
+++ main.js
@@ -11,6 +11,21 @@ const calcExpectedValue = () => {
     return avg / numThrows;
 }
 
+const calcExpectedValue2 = () => {
+    const numThrows = 100000;
+
+    let avg = 0;
+    for (let i = 0; i < numThrows; i++) {
+       let val = throwDie();
+       if (val < 5) {
+           val = Math.max(val, throwDie());
+       }
+       avg += val;
+    }
+
+    return avg / numThrows;
+}
+
 for (let i = 0; i < 5; i++) {
     console.log(calcExpectedValue());
 }
